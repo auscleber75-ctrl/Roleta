be8e21670474cc64dfabfaf15aa1ffe6618e4e13
auscleber75-ctrl <aus.cleber75@gmail.com>
2025-10-31 14:18:11 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,21 @@
+const CACHE_NAME = 'roleta-iap-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './IMG_4124.jpeg',
+  './IMG_4123.jpeg',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700;800&display=swap'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/index.html',
-        '/manifest.json',
-        '/images/icon.jpg’,
-        '/images/icon.jpg'
-        // Adicione outros arquivos necessários para o funcionamento offline
-      ]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
